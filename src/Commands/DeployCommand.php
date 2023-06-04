af2d07e184704d0c05ed7a29a676b10bca3fc900
Li Zhineng <im@zhineng.li>
2023-06-04 17:17:57 +0800

    extract project resolving

--- src/Commands/DeployCommand.php
+++ src/Commands/DeployCommand.php
@@ -2,6 +2,7 @@
 
 namespace Dew\Cli\Commands;
 
+use Dew\Cli\Concerns\ResolvesProject;
 use Dew\Cli\Credentials;
 use Dew\Cli\Deployment;
 use Dew\Cli\Project;
@@ -13,19 +14,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'deploy', description: 'Deploy the app to Alibaba Cloud')]
 class DeployCommand extends Command
 {
+    use ResolvesProject;
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // The project should be resolved from API endpoint dynamically based
-        // on the Dew token, but now in the early stage of development, lets
-        // use environment variables from shell instead for fast moving on.
-        $project = new Project(getenv('DEW_PROJECT_NAME'), getenv('DEW_PROJECT_REGION'));
-
-        $project->credentialsUsing(new Credentials(
-            getenv('ALI_KEY_ID'), getenv('ALI_KEY_SECRET'), getenv('ALI_ACCOUNT_ID')
-        ));
-
         $deployment = new Deployment(getcwd());
-        $deployment->projectUsing($project);
+        $deployment->projectUsing($this->project());
 
         $deployment->handle();
 
