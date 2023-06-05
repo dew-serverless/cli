618cba104e245fdb37e931b7513f539408cc3ce4
Li Zhineng <im@zhineng.li>
2023-06-05 12:53:20 +0800

    publishes handler.php

--- src/Deployments/PublishStubs.php
+++ src/Deployments/PublishStubs.php
@@ -16,6 +16,7 @@ class PublishStubs
 
         $filesystem->copy($stubs.'/.rr.yaml', $buildDir.'/.rr.yaml');
         $filesystem->copy($stubs.'/runtime.php', $buildDir.'/runtime.php');
+        $filesystem->copy($stubs.'/handler.php', $buildDir.'/handler.php');
 
         return $deployment;
     }
