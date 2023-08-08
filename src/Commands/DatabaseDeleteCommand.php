440d3c4a852605d07898dbcb08d463ba6a53807f
Li Zhineng <im@zhineng.li>
2023-08-08 19:10:16 +0800

    delete database instance command

---
+++ src/Commands/DatabaseDeleteCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Dew\Cli\Commands;
+
+use Dew\Cli\Configuration;
+use Dew\Cli\Database\DeleteDatabaseInstance;
+use Dew\Cli\ProjectConfig;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(name: 'db:delete', description: 'Delete a database instance')]
+class DatabaseDeleteCommand extends Command
+{
+    /**
+     * Configures the current command.
+     */
+    protected function configure(): void
+    {
+        $this->addArgument('name', InputArgument::OPTIONAL, 'The name of database instance');
+    }
+
+    /**
+     * Executes the current command.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $action = (new DeleteDatabaseInstance)
+            ->tokenUsing(Configuration::createFromEnvironment()->getToken())
+            ->forProject(ProjectConfig::load()->get('id'))
+            ->forInstance($input->getArgument('name') ?: $io->ask('What is the name of the database instance'));
+
+        if ($io->confirm('Do you want to delete the database instance')) {
+            $action->delete();
+
+            $io->success('The database instance has been deleted!');
+        }
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Database/DeleteDatabaseInstance.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Dew\Cli\Database;
+
+use Dew\Cli\Client;
+use Dew\Cli\InteractsWithDew;
+
+class DeleteDatabaseInstance
+{
+    use InteractsWithDew;
+
+    /**
+     * The project ID.
+     */
+    public int $projectId;
+
+    /**
+     * The name of the database instance.
+     */
+    public string $instance;
+
+    /**
+     * Configure project.
+     */
+    public function forProject(int $projectId): self
+    {
+        $this->projectId = $projectId;
+
+        return $this;
+    }
+
+    /**
+     * Configure database instance.
+     */
+    public function forInstance(string $name): self
+    {
+        $this->instance = $name;
+
+        return $this;
+    }
+
+    /**
+     * Delete the database instance.
+     */
+    public function delete(): void
+    {
+        Client::make()
+            ->delete('/api/projects/'.$this->projectId.'/databases/'.$this->instance, [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => sprintf('Bearer %s', $this->token),
+                ],
+                'json' => [
+                    'name' => $this->instance,
+                ],
+            ]);
+    }
+}
