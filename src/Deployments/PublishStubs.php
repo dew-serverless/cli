07fc4617c4fe800255bfef40bc138ae1a2ac7f98
Li Zhineng <im@zhineng.li>
2024-02-23 17:26:51 +0800

    fpm & cli runtime stubs

--- src/Deployments/PublishStubs.php
+++ src/Deployments/PublishStubs.php
@@ -15,9 +15,9 @@ class PublishStubs
         $stubs = $buildDir.'/vendor/dew/core/stubs';
 
         $filesystem->copy($stubs.'/runtime.php', $buildDir.'/runtime.php');
+        $filesystem->copy($stubs.'/fpmRuntime.php', $buildDir.'/fpmRuntime.php');
+        $filesystem->copy($stubs.'/cliRuntime.php', $buildDir.'/cliRuntime.php');
         $filesystem->copy($stubs.'/handler.php', $buildDir.'/handler.php');
-        $filesystem->copy($stubs.'/httpHandler.php', $buildDir.'/httpHandler.php');
-        $filesystem->copy($stubs.'/consoleHandler.php', $buildDir.'/consoleHandler.php');
 
         return $deployment;
     }
