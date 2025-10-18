b96fed0e6876e406b1f3eb909fced781b1440624
Li Zhineng <im@zhineng.li>
2025-10-18 09:00:09 +0800

    apply fixes by Rector

--- src/Commands/LoginCommand.php
+++ src/Commands/LoginCommand.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 #[AsCommand(name: 'login', description: 'Authenticate with Dew')]
 class LoginCommand extends Command
 {
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
